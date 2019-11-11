ffe7d8150af508ba424ac832161878bf6f9fab00
Zhangzhuo <zhangzhuo55555@163.com>
2019-11-11 17:47:38 +0800

    发端实现了分帧（32个符号为一帧，帧间间隔16个符号)

--- Channel.v
+++ Channel.v
@@ -7,39 +7,23 @@ module Channel(
 );
 
 
-reg signed [4:0] noise; //000 001 011 111 110 100 000
-reg [1:0] noise_count;  
-
-reg rest;
-reg [3:0] rest_count;
+reg signed [4:0] noise; //00000 00001 00011 00111 01111 11111 
 
 
 always@(posedge clk or negedge reset) begin
 	if(!reset) begin
 		noise <= 5'b00000;
-		noise_count <= 2'b00;
-		rest <= 1'b0;
-		rest_count <= 4'b0;
 		channel_out <= 9'd0;
 		end
 	else begin
-		if (noise_count == 2'b11) begin
-			if(IsTransmit && !rest) channel_out <= channel_in + noise;
-			else channel_out <= noise;
-		end
-		else begin
-			if(IsTransmit && !rest) channel_out <= channel_in;
-			else channel_out <= 9'd0;
-		end
-			
+		if(IsTransmit) channel_out <= channel_in + {{4{noise[4]}},noise};
+		else channel_out <= {{4{noise[4]}},noise};
 		
-		noise_count <= noise_count + 2'd1;
-		
-		noise[4] <= noise[2];
+		noise[4] <= noise[3];
 		noise[3] <= noise[2];
 		noise[2] <= noise[1];
 		noise[1] <= noise[0];
-		noise[0] <= !noise[3];
+		noise[0] <= !noise[4];
 	end
 end
 
--- QAM.v
+++ QAM.v
@@ -10,28 +10,53 @@ module QAM(
 
 
 reg [1:0] current_conv;
+reg signed [5:0] symbol_count;
 
+reg rest;
+reg [4:0] rest_count;
 
 always@(posedge clk or negedge reset) begin
 	if(~reset) begin
 		trans_read <= 7'd0;
 		current_conv <= 2'd0;
 		modulation_out <= 9'd0;
+		symbol_count <= -6'd1;
+		rest <= 1'b0;
+		rest_count <= 5'd0;
 	end
 	else begin
-		if(trans_read == 7'd0) begin
-			current_conv <= conv_in;
-			case(conv_in)
-				2'b00,2'b01:modulation_out <= GetCos;
-				2'b10,2'b11:modulation_out <= -GetCos;
-			endcase
+		if(trans_read == 7'd0) begin // starting time of a symbol
+			if(!rest) begin // Start Sending a new symbol if not resting
+				current_conv <= conv_in;
+				case(conv_in)
+					2'b00,2'b01:modulation_out <= GetCos;
+					2'b10,2'b11:modulation_out <= -GetCos;
+				endcase
+				if(symbol_count == 6'd31) begin  //Have a rest if having sent 32 symbols
+					rest <= 1'b1;
+					symbol_count <= 6'd0;
+				end
+				else symbol_count <= symbol_count + 6'd1;
+			end
+			else begin // if resting
+				modulation_out <= 9'd0;
+				if(rest_count == 5'd15) begin // Terminating the rest if having rest for 16 symbols 
+					rest <= 1'b0;
+					rest_count <= 5'd0;
+					symbol_count <= 6'd0;
+				end
+				else rest_count <= rest_count + 5'd1;
+			end
 		end
-		else case(current_conv)
+		else begin
+			if(!rest) case(current_conv)
 				 2'b00: modulation_out <= -GetSin + GetCos;
 				 2'b01: modulation_out <= GetSin + GetCos;
 				 2'b11: modulation_out <= GetSin - GetCos;
 				 2'b10: modulation_out <= -GetSin - GetCos;
-			  endcase
+			endcase
+			else modulation_out <= 9'd0;
+		end
 			
 		trans_read <= trans_read + 7'd1;
 	end
